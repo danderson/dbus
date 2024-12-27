a9261f860170d00830056324c7c73c6d8c07a065
David Anderson <dave@natulte.net>
2024-12-27 12:07:04 -0800

    make the common cache pull-through, and support unwinding
    Signed-off-by: David Anderson <dave@natulte.net>

--- caches.go
+++ caches.go
@@ -1,36 +1,76 @@
 package dbus
 
 import (
-	"fmt"
 	"reflect"
 	"sync"
 )
 
+// cache is a pull-through cache of values derived from reflected
+// types.
 type cache[V any] struct {
-	OnRecursive func(reflect.Type) V
-	m           sync.Map
+	get          func(reflect.Type) V
+	getRecursive func(reflect.Type) V
+	m            sync.Map
 }
 
-func (c *cache[V]) Get(t reflect.Type) (val V, found bool) {
+// Init sets the constructors to use on cache miss. The cache calls
+// get(t) to construct a value for type t, and getRecursive(t) when a
+// cache miss occurs for t while executing get(t).
+func (c *cache[V]) Init(get, getRecursive func(reflect.Type) V) {
+	c.get = get
+	c.getRecursive = getRecursive
+}
+
+// Get returns the value associated with t, constructing it if
+// necessary.
+func (c *cache[V]) Get(t reflect.Type) (ret V) {
 	ent, loaded := c.m.LoadOrStore(t, nil)
-	if !loaded {
-		var zero V
-		return zero, false
+	if loaded && ent != nil {
+		return ent.(V)
+
 	}
-	if ent == nil {
-		if c.OnRecursive == nil {
-			panic(unrepresentable(t, "recursive type"))
+	defer func() {
+		if e := recover(); e != nil {
+			if v, ok := e.(errUnwind[V]); ok {
+				c.m.CompareAndSwap(t, nil, v.v)
+			}
+			panic(e)
 		}
-		ret := c.OnRecursive(t)
-		c.m.CompareAndSwap(t, nil, ret)
-		return ret, true
+	}()
+
+	if loaded {
+		ret = c.getRecursive(t)
+	} else {
+		ret = c.get(t)
 	}
-	if val, ok := ent.(V); ok {
-		return val, true
-	}
-	panic(fmt.Sprintf("mystery value %v (%T) in cache", ent, ent))
+	c.m.CompareAndSwap(t, nil, ret)
+	return ret
 }
 
-func (c *cache[V]) Put(t reflect.Type, val V) {
-	c.m.CompareAndSwap(t, nil, val)
+// Unwind unwinds the call stack to the nearest [cache.GetRecover],
+// caching v as the result of all intermediate [cache.Get] calls.
+func (c *cache[V]) Unwind(v V) {
+	panic(errUnwind[V]{v})
+}
+
+// GetRecover is like Get, but stops the propagation of [cache.Unwind]
+// further up the stack. It returns either the result of Get, or if an
+// unwind occurs, the value passed to [cache.Unwind].
+func (c *cache[V]) GetRecover(t reflect.Type) (ret V) {
+	defer func() {
+		if e := recover(); e != nil {
+			if v, ok := e.(errUnwind[V]); ok {
+				// Get() already poked v into the cache, we just need
+				// to eat the panic.
+				ret = v.v
+			} else {
+				panic(e)
+			}
+		}
+	}()
+	return c.Get(t)
+}
+
+type errUnwind[V any] struct {
+	v V
 }
