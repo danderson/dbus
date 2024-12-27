a0ef4aa020463e6afb87c7265d7d8ac94eb68b79
David Anderson <dave@natulte.net>
2024-12-26 23:49:09 -0800

    move error type into its own file
    Signed-off-by: David Anderson <dave@natulte.net>

--- encode.go
+++ encode.go
@@ -9,23 +9,6 @@ import (
 	"sync"
 )
 
-type ErrUnrepresentable struct {
-	Type   string
-	Reason string
-}
-
-func (e ErrUnrepresentable) Error() string {
-	return fmt.Sprintf("dbus cannot represent %s: %s", e.Type, e.Reason)
-}
-
-func unrepresentable(t reflect.Type, reason string) error {
-	ts := ""
-	if t != nil {
-		ts = t.String()
-	}
-	return ErrUnrepresentable{ts, reason}
-}
-
 func Marshal(v any, ord binary.AppendByteOrder) ([]byte, error) {
 	return MarshalAppend(nil, v, ord)
 }
