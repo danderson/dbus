3df5dad5f426440c25257e1e0726ac9491a84c2f
David Anderson <dave@natulte.net>
2024-12-28 01:21:45 -0800

    add documentation for value marshaling
    Signed-off-by: David Anderson <dave@natulte.net>

--- encode.go
+++ encode.go
@@ -10,10 +10,57 @@ import (
 	"github.com/danderson/dbus/fragments"
 )
 
+// Marshal returns the DBus wire encoding of v, using the given byte
+// ordering.
+//
+// Marshal traverses the value v recursively. If an encountered value
+// implements [Marshaler], Marshal calls [Marshaler.MarshalDBus] to
+// produce its encoding.
+//
+// Otherwise, Marshal uses the following type-dependent default
+// encodings:
+//
+// Integer, boolean, float64, and string values encode to their
+// corresponding DBus basic types. float32 values encode like float64.
+//
+// Array and slice values encode as DBus arrays. Nil slices encode the
+// same as an empty slice.
+//
+// Struct values encode as DBus structs. Each exported struct field is
+// encoded in declaration order, according to its own type. Embedded
+// struct fields are encoded as if their inner exported fields were
+// fields in the outer struct, subject to the usual Go visibility
+// rules.
+//
+// Map values encode as a DBus dictionary, i.e. an array of key/value
+// pairs. The map's key type must be a number, boolean or string.
+//
+// Pointer values encode as the value pointed to. A nil pointer
+// encodes as the zero value of the type pointed to.
+//
+// [Signature], [ObjectPath], and [FileDescriptor] values encode to
+// the corresponding DBus types.
+//
+// [Variant] values encode as DBus variants. The Variant's inner value
+// must be a value acceptable to Marshal, or it will return an
+// [ErrUnrepresentable].
+//
+// Interface, channel, complex, and function values cannot be
+// encoded. Attempting to encode such values causes Marshal to return
+// an [ErrUnrepresentable].
+//
+// DBus cannot represent cyclic or recursive types, and Marshal does
+// not handle them. Attempting to encode such values causes Marshal to
+// return an [ErrUnrepresentable].
 func Marshal(v any, ord binary.AppendByteOrder) ([]byte, error) {
 	return MarshalAppend(nil, v, ord)
 }
 
+// MarshalAppend is like [Marshal] but appends its output to bs and
+// returns the extended slice.
+//
+// Additional padding bytes may be appended prior to the start of the
+// marshaled value, to respect DBus alignment rules.
 func MarshalAppend(bs []byte, v any, ord binary.AppendByteOrder) ([]byte, error) {
 	val := reflect.ValueOf(v)
 	enc := encoders.GetRecover(val.Type())
@@ -28,6 +75,8 @@ func MarshalAppend(bs []byte, v any, ord binary.AppendByteOrder) ([]byte, error)
 	return st.Out, nil
 }
 
+// debugEncoders enables spammy debug logging during the construction
+// of encoder funcs.
 const debugEncoders = false
 
 func debugEncoder(msg string, args ...any) {
@@ -37,6 +86,15 @@ func debugEncoder(msg string, args ...any) {
 	log.Printf(msg, args...)
 }
 
+// Marshaler is the interface implemented by types that can marshal
+// themselves to the DBus wire format.
+//
+// The [Signature] and alignment returned by Marshalers may vary
+// depending on the value being encoded, but a given value must have a
+// consistent signature and alignment.
+//
+// [Marshaler.MarshalDBus] may assume that the output has already been
+// padded according to the value returned by [Marshaler.AlignDBus].
 type Marshaler interface {
 	SignatureDBus() Signature
 	AlignDBus() int
@@ -56,10 +114,14 @@ func init() {
 	})
 }
 
+// uncachedTypeEncoder returns the EncoderFunc for t.
 func uncachedTypeEncoder(t reflect.Type) (ret fragments.EncoderFunc) {
 	debugEncoder("typeEncoder(%s)", t)
 	defer debugEncoder("end typeEncoder(%s)", t)
 
+	// If a value's pointer type implements Unmarshaler, we can avoid
+	// a value copy by using it. But we can only use it for
+	// addressable values, which requires an additional runtime check.
 	if t.Kind() != reflect.Pointer && reflect.PointerTo(t).Implements(marshalerType) {
 		return newCondAddrMarshalEncoder(t)
 	} else if t.Implements(marshalerType) {
@@ -91,6 +153,30 @@ func uncachedTypeEncoder(t reflect.Type) (ret fragments.EncoderFunc) {
 	return newErrEncoder(t, "no known mapping")
 }
 
+// newErrEncoder signals that the requested type cannot be encoded to
+// the DBus wire format for the given reason.
+//
+// Internally the function triggers an unwind back to Marshal, caching
+// the error with all intermediate EncoderFuncs. This saves time
+// during decoding because Marshal can return an error immediately,
+// rather than get halfway through a complex object only to discover
+// that it cannot be encoded.
+//
+// However, the semantics are equivalent to returning the error
+// encoder normally, so callers may use this function like any other
+// EncoderFunc constructor.
+func newErrEncoder(t reflect.Type, reason string) fragments.EncoderFunc {
+	err := unrepresentable(t, reason)
+	encoders.Unwind(func(*fragments.Encoder, reflect.Value) error {
+		return err
+	})
+	// So that callers can return the result of this constructor and
+	// pretend that it's not doing any non-local return. The non-local
+	// return is just an optimization so that encoders don't waste
+	// time partially encoding types that will never fully succeed.
+	return nil
+}
+
 func newCondAddrMarshalEncoder(t reflect.Type) fragments.EncoderFunc {
 	ptr := newMarshalEncoder(reflect.PointerTo(t))
 	if t.Implements(marshalerType) {
@@ -112,18 +198,6 @@ func newCondAddrMarshalEncoder(t reflect.Type) fragments.EncoderFunc {
 			return ptr(st, v.Addr())
 		}
 	}
-}
-
-func newErrEncoder(t reflect.Type, reason string) fragments.EncoderFunc {
-	err := unrepresentable(t, reason)
-	encoders.Unwind(func(*fragments.Encoder, reflect.Value) error {
-		return err
-	})
-	// So that callers can return the result of this constructor and
-	// pretend that it's not doing any non-local return. The non-local
-	// return is just an optimization so that encoders don't waste
-	// time partially encoding types that will never fully succeed.
-	return nil
 }
 
 func newMarshalEncoder(t reflect.Type) fragments.EncoderFunc {
@@ -250,6 +324,7 @@ func newStringEncoder() fragments.EncoderFunc {
 
 func newSliceEncoder(t reflect.Type) fragments.EncoderFunc {
 	if t.Elem().Kind() == reflect.Uint8 {
+		// Fast path for []byte
 		debugEncoder("[]byte{}")
 		return func(st *fragments.Encoder, v reflect.Value) error {
 			bs := v.Bytes()
@@ -267,8 +342,13 @@ func newSliceEncoder(t reflect.Type) fragments.EncoderFunc {
 		ln := v.Len()
 		st.Pad(4)
 		st.Uint32(uint32(ln))
+		// Per the spec, zero length arrays must still insert padding
+		// after the array length, to match the alignment of the
+		// non-existent array entries.
 		st.Pad(arrayPad(t.Elem()))
 		for i := 0; i < ln; i++ {
+			// For non-empty arrays, element encoders provide their
+			// own alignment padding between elements.
 			if err := elemEnc(st, v.Index(i)); err != nil {
 				return err
 			}
@@ -277,6 +357,7 @@ func newSliceEncoder(t reflect.Type) fragments.EncoderFunc {
 	}
 }
 
+// arrayPad returns the correct byte alignment for elem.
 func arrayPad(elem reflect.Type) int {
 	if elem.Implements(marshalerType) {
 		return reflect.Zero(elem).Interface().(Marshaler).AlignDBus()
