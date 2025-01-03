bfae26c4d14020087b9fc0f5f07431d0883a74f2
David Anderson <dave@natulte.net>
2025-01-03 15:55:35 -0800

    Add methods to path, clean paths when handling them
    Signed-off-by: David Anderson <dave@natulte.net>

--- path.go
+++ path.go
@@ -2,7 +2,9 @@ package dbus
 
 import (
 	"context"
+	"path"
 	"reflect"
+	"strings"
 
 	"github.com/danderson/dbus/fragments"
 )
@@ -10,7 +12,7 @@ import (
 type ObjectPath string
 
 func (p ObjectPath) MarshalDBus(ctx context.Context, st *fragments.Encoder) error {
-	st.Value(ctx, string(p))
+	st.Value(ctx, string(p.Clean()))
 	return nil
 }
 
@@ -19,7 +21,7 @@ func (p *ObjectPath) UnmarshalDBus(ctx context.Context, st *fragments.Decoder) e
 	if err := st.Value(ctx, &s); err != nil {
 		return err
 	}
-	*p = ObjectPath(s)
+	*p = ObjectPath(s).Clean()
 	return nil
 }
 
@@ -28,3 +30,24 @@ func (p ObjectPath) IsDBusStruct() bool { return false }
 var objectPathSignature = mkSignature(reflect.TypeFor[ObjectPath]())
 
 func (p ObjectPath) SignatureDBus() Signature { return objectPathSignature }
+
+func (p ObjectPath) Clean() ObjectPath {
+	return ObjectPath(path.Clean(string(p)))
+}
+
+func (p ObjectPath) String() string {
+	return string(p.Clean())
+}
+
+func (p ObjectPath) Valid() bool {
+	return path.IsAbs(string(p.Clean()))
+}
+
+func (p ObjectPath) IsChildOf(parent ObjectPath) bool {
+	sparent := string(parent.Clean())
+	sp := string(p.Clean())
+	if len(sp) <= len(sparent) {
+		return false
+	}
+	return strings.HasPrefix(sp, sparent+"/")
+}
