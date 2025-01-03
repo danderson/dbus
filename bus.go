49fcb48f8d001fdbbd46201437e4247f6175eafc
David Anderson <dave@natulte.net>
2025-01-03 09:48:31 -0800

    Move base bus signal types to bus.go
    This leaves signal.go free for the machinery of handling all signals,
    without clutter from the particular bus signals we implement directly.
    
    Signed-off-by: David Anderson <dave@natulte.net>

--- bus.go
+++ bus.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"maps"
+
+	"github.com/creachadair/mds/mapset"
+	"github.com/danderson/dbus/fragments"
 )
 
 type NameRequestFlags byte
@@ -105,3 +109,148 @@ func (c *Conn) Features(ctx context.Context, opts ...CallOption) ([]string, erro
 // TODO:
 //  - AddMatch/RemoveMatch: should be internal only, behind a nicer
 //    signals monitoring API.
+
+type NameOwnerChanged struct {
+	Name string
+	Prev *Peer
+	New  *Peer
+}
+
+func (s *NameOwnerChanged) IsDBusStruct() bool { return true }
+
+func (s *NameOwnerChanged) SignatureDBus() Signature { return mustParseSignature("sss") }
+
+func (s *NameOwnerChanged) UnmarshalDBus(ctx context.Context, d *fragments.Decoder) error {
+	var body struct {
+		Name, Prev, New string
+	}
+	if err := d.Value(ctx, &body); err != nil {
+		return err
+	}
+
+	sender, ok := ContextSender(ctx)
+	if !ok {
+		return errors.New("can't unmarshal NameOwnerChanged signal, no sender in context")
+	}
+
+	s.Name = body.Name
+	if body.Prev != "" {
+		p := sender.Conn().Peer(body.Prev)
+		s.Prev = &p
+	}
+	if body.New != "" {
+		n := sender.Conn().Peer(body.New)
+		s.New = &n
+	}
+
+	return nil
+}
+
+type NameLost struct {
+	Name string
+}
+
+type NameAcquired struct {
+	Name string
+}
+
+type ActivatableServicesChanged struct{}
+
+type PropertiesChanged struct {
+	Interface   Interface
+	Changed     map[string]any
+	Invalidated mapset.Set[string]
+}
+
+func (s *PropertiesChanged) IsDBusStruct() bool { return true }
+
+func (s *PropertiesChanged) SignatureDBus() Signature { return mustParseSignature("sa{sv}as") }
+
+func (s *PropertiesChanged) UnmarshalDBus(ctx context.Context, d *fragments.Decoder) error {
+	var body struct {
+		Interface   string
+		Changed     map[string]Variant
+		Invalidated []string
+	}
+	if err := d.Value(ctx, &body); err != nil {
+		return err
+	}
+
+	sender, ok := ContextSender(ctx)
+	if !ok {
+		return errors.New("can't unmarshal PropertiesChanged signal, no sender in context")
+	}
+
+	s.Interface = sender.Object().Interface(body.Interface)
+	s.Changed = map[string]any{}
+	for k, v := range body.Changed {
+		s.Changed[k] = v.Value
+	}
+	s.Invalidated = mapset.New(body.Invalidated...)
+
+	return nil
+}
+
+type InterfacesAdded struct {
+	Object     Object
+	Interfaces []Interface
+}
+
+func (s *InterfacesAdded) IsDBusStruct() bool { return true }
+
+func (s *InterfacesAdded) SignatureDBus() Signature { return mustParseSignature("oa{sa{sv}}") }
+
+func (s *InterfacesAdded) UnmarshalDBus(ctx context.Context, d *fragments.Decoder) error {
+	var body struct {
+		Path        ObjectPath
+		IfsAndProps map[string]map[string]Variant
+	}
+	if err := d.Value(ctx, &body); err != nil {
+		return err
+	}
+
+	sender, ok := ContextSender(ctx)
+	if !ok {
+		return errors.New("can't unmarshal InterfacesAdded signal, no sender in context")
+	}
+
+	// TODO: check path is a child of iface.Object()
+	s.Object = sender.Peer().Object(body.Path)
+	s.Interfaces = s.Interfaces[:0]
+	for k := range maps.Keys(body.IfsAndProps) {
+		s.Interfaces = append(s.Interfaces, s.Object.Interface(k))
+	}
+
+	return nil
+}
+
+type InterfacesRemoved struct {
+	Object     Object
+	Interfaces []Interface
+}
+
+func (s *InterfacesRemoved) IsDBusStruct() bool { return true }
+
+func (s *InterfacesRemoved) SignatureDBus() Signature { return mustParseSignature("oa{sa{sv}}") }
+
+func (s *InterfacesRemoved) UnmarshalDBus(ctx context.Context, d *fragments.Decoder) error {
+	var body struct {
+		Path ObjectPath
+		Ifs  []string
+	}
+	if err := d.Value(ctx, &body); err != nil {
+		return err
+	}
+
+	sender, ok := ContextSender(ctx)
+	if !ok {
+		return errors.New("can't unmarshal InterfacesRemoved signal, no sender in context")
+	}
+
+	s.Object = sender.Peer().Object(body.Path)
+	s.Interfaces = s.Interfaces[:0]
+	for _, iface := range body.Ifs {
+		s.Interfaces = append(s.Interfaces, s.Object.Interface(iface))
+	}
+	return nil
+}
