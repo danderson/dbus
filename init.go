13e3053b4aeb153cf6030504856e725e0b38167f
David Anderson <dave@natulte.net>
2025-01-12 09:54:02 -0800

    Fix the signal interface name for InterfacesAdded/Removed.
    Signed-off-by: David Anderson <dave@natulte.net>

--- init.go
+++ init.go
@@ -5,7 +5,9 @@ func init() {
 	RegisterSignalType[NameLost]("org.freedesktop.DBus", "NameLost")
 	RegisterSignalType[NameAcquired]("org.freedesktop.DBus", "NameAcquired")
 	RegisterSignalType[ActivatableServicesChanged]("org.freedesktop.DBus", "ActivatableServicesChanged")
+
 	RegisterSignalType[PropertiesChanged]("org.freedesktop.DBus.Properties", "PropertiesChanged")
-	RegisterSignalType[InterfacesAdded]("org.freedesktop.DBus", "InterfacesAdded")
-	RegisterSignalType[InterfacesRemoved]("org.freedesktop.DBus", "InterfacesRemoved")
+
+	RegisterSignalType[InterfacesAdded]("org.freedesktop.DBus.ObjectManager", "InterfacesAdded")
+	RegisterSignalType[InterfacesRemoved]("org.freedesktop.DBus.ObjectManager", "InterfacesRemoved")
 }
